4c8bfc13743f0741f915afcd13f85445f118c75e
Ryan Jannik <runtisz@me.com>
2022-06-04 12:40:32 -0600

    updated personal finances file

--- personal finances.py
+++ personal finances.py
@@ -1,13 +1,14 @@
-old_salary = 79100
-old_net_mo = 4632
-pct_takehome = old_net_mo/(old_salary/12)
-salary = 92000*1.1
-projected_monthly = salary * pct_takehome / 12
+salary = 92000
+monthly_salary = 5300
 
-rent_future =  1300
-home_expenses =  231.56/2
-credit_bills =  350
-loan_payments =  1645-610
+print(monthly_salary)
+
+rent_future =  1795
+home_expenses =  50+100+60
+credit_bills =  200
+loan_payments =  610+333+440+250
+
+print(loan_payments+credit_bills)
 
 def required_exp(rent_future, home_expenses, loan_payments, credit_bills):
    return rent_future+home_expenses+loan_payments+credit_bills
@@ -16,13 +17,13 @@ def discretionary(money_in, money_out):
    return money_in - money_out
 
 total_expense = required_exp(rent_future,home_expenses,loan_payments,credit_bills)
-usable_balance = discretionary(projected_monthly, total_expense)
+usable_balance = discretionary(monthly_salary, total_expense)
 
 food = 500
 fuel = 150
-general = 350
-insurance = 300
-phone = 150
+general = 250
+insurance = 54+150+25+17
+phone = 180
 
 non_essential_exp = food+fuel+general+insurance+phone
 
